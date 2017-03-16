044802a7dcb65716a16353dba5e4790cfc2a05ac
Pubudu Kodikara <pubudu@leafycode.com>
2017-03-16 18:17:16 +0530

    Complete chapter 3

---
+++ src/server/render-app.js
@@ -0,0 +1,19 @@
+// @flow
+import { html } from 'common-tags'
+
+import { STATIC_PATH } from '../shared/config'
+
+const renderApp = (title: string) =>
+html`<!doctype html>
+<html>
+  <head>
+    <title>${title}</title>
+    <link rel="stylesheet" href="${STATIC_PATH}/css/style.css">
+  </head>
+  <body>
+    <h1>${title}</h1>
+  </body>
+</html>
+`
+
+export default renderApp
